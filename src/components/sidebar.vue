7d4fdb4d13a1eda2f54626bb87bdd0668c78a942
antwanola <antwanola29@gmail.com>
2022-10-15 19:48:12 +0100

    add new menus and button transition

--- src/components/sidebar.vue
+++ src/components/sidebar.vue
@@ -1,7 +1,7 @@
 <template>
-  <aside :class="`${expanded && 'is_expanded'}`">
+  <aside :class="`${expanded && 'is_expanded'}`" >
     <div class="logo">
-      <img src="../assets/vue.svg" alt="logo">     
+      <img src="../assets/vue.svg" alt="logo"/>     
     </div>
     <div class="menu-toggle-wrap"  @click="toggleMenu">
       <button class="menu-toggle-class">
@@ -10,18 +10,39 @@
           </span>
       </button>
     </div>
+    <h4>Menu</h4>
+  <div class="menu">
+    <router-link class="button" to="/">
+      <span class="material-icons">home</span>
+      <span class="text">Home</span>
+    </router-link>
+
+    <router-link class="button" to="/about">
+      <span class="material-icons">assignment</span>
+      <span class="text">About</span>
+    </router-link>
+
+    <router-link class="button" to="/">
+      <span class="material-icons">visibility</span>
+      <span class="text">Team</span>
+    </router-link>
+
+    <router-link class="button" to="/">
+      <span class="material-icons">email</span>
+      <span class="text">contact us</span>
+    </router-link>
+  </div>
   </aside>
+
+  
 </template>
 
 <script setup>
 import { ref } from "vue"
  const expanded = ref (false)
 
- const toggleMenu = () =>{
-  expanded.value =  !expanded.value
- }
+ const toggleMenu = () => expanded.value =  !expanded.value
+
 
 </script>
 
-<style lang="scss" scoped>
-</style>import logo from "../assets/vue.svg"
